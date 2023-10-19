d5015b43ed925b0fd1b8f050736c2f689e5b8cd2
John Conley <8932043+jfrconley@users.noreply.github.com>
2023-10-19 15:50:22 -0700

    bug fixes and remote migrations support

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -8,10 +8,10 @@ terraform {
 
 resource "cockroach-extra_migration" "cool-migration" {
   cluster_id           = "29fd13f0-32a0-42e6-bf51-e7c3c5bbd03e"
-  migrations_directory = "/Users/john/workspace/terraform-provider-cockroach-extra/examples/provider/migrations"
+  migrations_url = "file://migrations"
   destroy_mode         = "drop"
-  database             = "defaultdb"
-  version              = "20231019025716"
+  database             = "migration-test"
+  version              = 20231019210913
 }
 
 provider "cockroach-extra" {
